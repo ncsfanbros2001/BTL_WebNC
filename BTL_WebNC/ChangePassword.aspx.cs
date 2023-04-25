ca987c8d466bde3f302320356f9d25663a368f4a
hanntn289 <95235063+hanntn289@users.noreply.github.com>
2023-04-25 18:35:36 +0700

    changePW

---
+++ BTL_WebNC/ChangePassword.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BTL_WebNC
+{
+    public partial class ChangePassword : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ID.Text = Session["ID"].ToString();
+        }
+
+        protected void Change_Click(object sender, EventArgs e)
+        {
+         
+            if (OldPassword.Text == Session["Password"].ToString())
+            {
+                SqlConnection con = new SqlConnection(@"SERVER=(localdb)\MSSQLLocalDB; DATABASE=BTL_WebNC;");
+                SqlCommand cmd = new SqlCommand("sp_changePW", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@ID", ID.Text);
+                cmd.Parameters.AddWithValue("@Password", NewPassword.Text);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                
+            }
+            else
+            {
+                note.InnerText = "mat khau khong dung";
+            }
+        }
+    }
+}
